11351911d47ca7c15ef6f3f556afcc309fc1f475
Gautier <gautier@apparence.io>
2022-01-24 11:15:57 +0100

    add wac example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -101,6 +101,15 @@ class _HomePageState extends State<HomePage> {
               leading: const Icon(Icons.video_camera_back),
               onTap: () => _showMeeriadDemoPopup(),
             ),
+            const Divider(),
+            ListTile(
+              title: const Text('Demo - Wac'),
+              subtitle: const Text(
+                'Single choice - Video then request user feedback',
+              ),
+              leading: const Icon(Icons.video_camera_back),
+              onTap: () => _showWacDemoPopup(),
+            ),
           ],
         ),
       ),
@@ -153,6 +162,23 @@ class _HomePageState extends State<HomePage> {
         Choice(id: 'a', text: 'La gestion des challenges'),
         Choice(id: 'b', text: 'L\'ajout de thématiques'),
         Choice(id: 'c', text: 'L\'amélioration des événements'),
+      ],
+      onTapChoice: (choice) {},
+      onVideoEndAction: () {},
+    );
+  }
+
+  Future _showWacDemoPopup() {
+    return PalPlugin.instance.showSingleChoiceSurvey(
+      context: context,
+      videoAsset: 'assets/wac.mp4',
+      userName: 'David',
+      companyTitle: 'Product manager - Wac',
+      question: 'Quelle fonctionnalité aimeriez-vous avoir prochainement ?',
+      choices: const [
+        Choice(id: 'a', text: 'Transfère d\'argent à un proche'),
+        Choice(id: 'b', text: 'Avoir un chat vidéo'),
+        Choice(id: 'c', text: 'Avoir un certificat de formation'),
       ],
       onTapChoice: (choice) {},
       onVideoEndAction: () {},
